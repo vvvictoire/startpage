563fe92f75caef0dc035cc29d7515a0bf17e77db
Victoire <vvictoire@pm.me>
2021-04-05 12:34:07 +0200

    Put the redirections in its own function, and append maths results

--- assets/js/victoire_startpage.js
+++ assets/js/victoire_startpage.js
@@ -10,25 +10,34 @@ function parseSearchQuery(search_query){
 	return {bang: bang, query: query};
 };
 
+function redirect(bang, query){
+	//bang: a single word, starting with a '!'
+	//query: a string of word(s)
+	switch(bang){
+		case '!q':
+			location.href = 'https://duckduckgo.com/?q=' + query;
+			break;
+		case '!bang':
+			location.href = 'https://duckduckgo.com/bang#bangs-list';
+			break;
+	}
+}
+
 form.addEventListener('submit', event => {
 	// submit event detected
 	event.preventDefault()
 	var search_query = $("#search_field").val();
 	if (search_query !== '') {
+		// if the search_query starts with a '!', then we’ll redirect
 		if (search_query[0] === '!'){
 			var bang_query = parseSearchQuery(search_query);
-			switch(bang_query.bang){
-				case '!q':
-					location.href = 'https://duckduckgo.com/?q=' + bang_query.query;
-					break;
-				case '!bang':
-					location.href = 'https://duckduckgo.com/bang#bangs-list';
-					break;
-			}
+			redirect(bang_query.bang, bang_query.query);
 		}
+		// if the search_query doesn’t start with a '!', then we evaluate as a maths expression
 		else {
 			var result = math.evaluate(search_query);
-			$("#result_field").html(search_query + " = " + result);
+			var old_result_field = $("#result_field").html();
+			$("#result_field").html(old_result_field + "<br/>" + search_query + " = " + result);
 			$("#search_field").val('');
 		}
 	}
